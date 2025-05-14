c1aba655a27b8c1caec0b12e87611a932401181a
Davixo23 <david.utic@gmail.com>
2025-05-14 11:38:08 -0400

    puntos

--- script.sh
+++ script.sh
@@ -11,3 +11,7 @@ sudo apt install nodejs
 
 # verificar que apache esta en ejecucion
 sudo systemctl status apache2
+
+# instalar git 
+
+# descargar un repo git que tenga html css y js en la carpeta de apache por defecto
